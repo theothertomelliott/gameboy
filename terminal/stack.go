235a8252a070c203224e51a9f85277c0684f49d3
Tom Elliott <tom.w.elliott@gmail.com>
2019-01-26 19:09:04 -0500

    Improve stack output in terminal

--- terminal/stack.go
+++ terminal/stack.go
@@ -2,6 +2,7 @@ package terminal
 
 import (
 	"fmt"
+	"sort"
 
 	"github.com/gdamore/tcell"
 	"github.com/rivo/tview"
@@ -14,13 +15,18 @@ type StackTable struct {
 
 	gb *gameboy.DMG
 
-	pushCommand map[uint16]uint16
+	stackEvents map[uint16]gameboy.StackEvent
+
+	pushCommand   map[uint16]uint16
+	pushOperation map[uint16]int64
 }
 
 func NewStackTable(gb *gameboy.DMG) *StackTable {
 	st := &StackTable{
-		gb:          gb,
-		pushCommand: make(map[uint16]uint16),
+		gb:            gb,
+		stackEvents:   make(map[uint16]gameboy.StackEvent),
+		pushCommand:   make(map[uint16]uint16),
+		pushOperation: make(map[uint16]int64),
 	}
 	st.Table = tview.NewTable().
 		SetBorders(false).
@@ -31,9 +37,11 @@ func NewStackTable(gb *gameboy.DMG) *StackTable {
 
 func (s *StackTable) Update() {
 	cpu := s.gb.CPU()
-	mmu := s.gb.MMU()
+	//mmu := s.gb.MMU()
 
-	psPos := cpu.SP.Read16()
+	spPos := cpu.SP.Read16()
+
+	s.Clear()
 
 	s.SetCell(
 		0,
@@ -57,27 +65,48 @@ func (s *StackTable) Update() {
 			SetAlign(tview.AlignCenter),
 	)
 
+	var positions []uint16
+	for pos := range s.stackEvents {
+		positions = append(positions, pos)
+	}
+	sort.Slice(positions, func(i int, j int) bool {
+		return positions[i] > positions[j]
+	})
+
 	var row int
-	for pos := uint16(0xFFFE); pos >= psPos; pos-- {
-		value := mmu.Read8(pos)
+	var wroteSP bool
+	for _, pos := range positions {
+		if pos < spPos && !wroteSP {
+			s.SetCell(
+				row+1,
+				0,
+				tview.NewTableCell("SP").
+					SetTextColor(tcell.ColorYellow).
+					SetAlign(tview.AlignCenter),
+			)
+			row++
+			wroteSP = true
+			continue
+		}
+
+		ev := s.stackEvents[pos]
 		s.SetCell(
 			row+1,
 			0,
-			tview.NewTableCell(fmt.Sprintf("0x%04X", pos)).
+			tview.NewTableCell(fmt.Sprintf("0x%04X", ev.Pos)).
 				SetTextColor(tcell.ColorYellow).
 				SetAlign(tview.AlignCenter),
 		)
 		s.SetCell(
 			row+1,
 			1,
-			tview.NewTableCell(fmt.Sprintf("0x%02X", value)).
+			tview.NewTableCell(fmt.Sprintf("0x%04X", ev.ValueIn)).
 				SetTextColor(tcell.ColorWhite).
 				SetAlign(tview.AlignCenter),
 		)
-		pushText := ""
-		if row%2 == 0 {
-			pushText = fmt.Sprintf("0x%04X", s.pushCommand[pos])
-		}
+
+		pushText := fmt.Sprintf("0x%04X", s.pushCommand[pos])
+		opText := fmt.Sprintf("0x%04X", s.pushOperation[pos])
 		s.SetCell(
 			row+1,
 			2,
@@ -85,14 +114,24 @@ func (s *StackTable) Update() {
 				SetTextColor(tcell.ColorWhite).
 				SetAlign(tview.AlignCenter),
 		)
+		s.SetCell(
+			row+1,
+			3,
+			tview.NewTableCell(opText).
+				SetTextColor(tcell.ColorWhite).
+				SetAlign(tview.AlignCenter),
+		)
 		row++
 	}
+	s.ScrollToEnd()
 }
 
 func (s *StackTable) Trace(ev gameboy.TraceMessage) {
 	for _, stackOp := range ev.Stack {
 		if stackOp.ValueIn != 0 {
+			s.stackEvents[stackOp.Pos] = stackOp
 			s.pushCommand[stackOp.Pos] = ev.CPU.PC
+			s.pushOperation[stackOp.Pos] = ev.Count
 		}
 	}
 }
