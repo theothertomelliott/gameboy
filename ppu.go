d48970391446f6e76b7619487d0be4c0e65b992b
Tom Elliott <tom.w.elliott@gmail.com>
2019-06-19 22:49:18 -0400

    Remove more unused code.

--- ppu.go
+++ ppu.go
@@ -336,26 +336,3 @@ func getTile(number byte, tileMap []byte) []byte {
 	start := uint16(number) * 16
 	return tileMap[start : start+16]
 }
-
-// Detail at: http://bgb.bircd.org/pandocs.htm#videodisplay
-
-// Tile pattern tables & Sprite Pattern Table
-//  $8000-8FFF -	sprites, the background, and the window display
-//					Its tiles are numbered from 0 to 255.
-//  $8800-97FF - The second table can be used for the background and
-//				 the window display and its tiles are numbered from
-//				 -128 to 127.
-// Background Tile Map
-
-// Tile background maps
-//  $9800-9BFF
-//  $9C00-9FFF
-
-// Sprite attributes reside in the Sprite Attribute Table (OAM - Object Attribute Memory) at $FE00-FE9F
-
-var (
-	OAM = Range{
-		Start: 0xFE00,
-		End:   0xFE9F,
-	}
-)
