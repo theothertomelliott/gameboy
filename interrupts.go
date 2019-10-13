83dd1ff0a86c0cf60068fe4cec1f52f949c22cbe
Tom Elliott <tom.w.elliott@gmail.com>
2019-10-12 21:41:41 -0400

    Clean up interrupt handling
    Rely on IF to track enabled interrupts.

--- interrupts.go
+++ interrupts.go
@@ -1,7 +1,5 @@
 package gameboy
 
-import "fmt"
-
 type Interrupt struct {
 	Bit byte
 	ISR uint16
@@ -28,6 +26,13 @@ var (
 		Bit: 4,
 		ISR: 0x0060,
 	}
+	allInterrupts = []Interrupt{
+		InterruptVBlank,
+		InterruptLCDStatus,
+		InterruptTimerOverflow,
+		InterruptSerialLink,
+		InterruptJoypadPress,
+	}
 )
 
 func NewInterruptScheduler(cpu *CPU, mmu *MMU) *InterruptScheduler {
@@ -38,9 +43,8 @@ func NewInterruptScheduler(cpu *CPU, mmu *MMU) *InterruptScheduler {
 }
 
 type InterruptScheduler struct {
-	scheduledInterrupt *Interrupt
-	cpu                *CPU
-	mmu                *MMU
+	cpu *CPU
+	mmu *MMU
 }
 
 func (s *InterruptScheduler) ScheduleInterrupt(i Interrupt) {
@@ -48,43 +52,34 @@ func (s *InterruptScheduler) ScheduleInterrupt(i Interrupt) {
 		return
 	}
 
-	if s.scheduledInterrupt != nil {
-		fmt.Println("interrupt scheduled before previous interrupt was processed")
-	}
-
 	// Set the VBlank bit in IF to request an interrupt
 	ifValue := s.mmu.Read8(IF)
 	s.mmu.Write8(IF, setBitValue(i.Bit, ifValue, true))
-	s.scheduledInterrupt = &i
 }
 
 func (s *InterruptScheduler) HandleInterrupts() {
-	if !s.cpu.IME || s.scheduledInterrupt == nil {
-		return
-	}
-
-	// Clear interrupt
-	defer func() {
-		s.scheduledInterrupt = nil
-	}()
-
-	i := s.scheduledInterrupt
 	ieValue := s.mmu.Read8(IE)
 	ifValue := s.mmu.Read8(IF)
-	if bitValue(i.Bit, ieValue&ifValue) != 1 {
+
+	if !s.cpu.IME || ieValue&ifValue == 0 {
 		return
 	}
 
-	// Reset the bit in IF (the request)
-	s.mmu.Write8(IF, setBitValue(i.Bit, ifValue, false))
+	for _, i := range allInterrupts {
+		if bitValue(i.Bit, ieValue) != 0 && bitValue(i.Bit, ifValue) != 0 {
+			// Reset the bit in IF (the request)
+			s.mmu.Write8(IF, setBitValue(i.Bit, ifValue, false))
 
-	// CALL interrupt vector
-	s.cpu.CALL(Direct16(i.ISR))
+			// Disable interrupts
+			s.cpu.DI()
 
-	if s.cpu.isHalted {
-		s.cpu.isHalted = false
+			// CALL interrupt vector
+			s.cpu.CALL(Direct16(i.ISR))
+
+			if s.cpu.isHalted {
+				s.cpu.isHalted = false
+			}
+			return
+		}
 	}
-
-	// Disable interrupts
-	s.cpu.DI()
 }
