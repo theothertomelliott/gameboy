b96b3f59c87e0f214c8cdf34838f82c88e13834e
Tom Elliott <tom.w.elliott@gmail.com>
2019-10-16 23:21:41 -0400

    Clean up LCDControl comments

--- memorymap.go
+++ memorymap.go
@@ -1,21 +1,15 @@
 package gameboy
 
-/*
-Bit7  LCD operation                           | ON            | OFF
-Bit6  Window Tile Table address               | 9C00-9FFF     | 9800-9BFF
-Bit5  Window display                          | ON            | OFF
-Bit4  Tile Pattern Table address              | 8000-8FFF     | 8800-97FF
-Bit3  Background Tile Table address           | 9C00-9FFF     | 9800-9BFF
-Bit2  Sprite size                             | 8x16          | 8x8
-Bit1  Color #0 transparency in the window     | SOLID         | TRANSPARENT
-Bit0  Background display
-*/
 type LCDControl byte
 
+// LCDOperation ...
+// Bit7  LCD operation                           | ON            | OFF
 func (l LCDControl) LCDOperation() bool {
 	return bitValue(7, byte(l)) != 0
 }
 
+// WindowTileTableAddress ...
+// Bit6  Window Tile Table address               | 9C00-9FFF     | 9800-9BFF
 func (l LCDControl) WindowTileTableAddress() Range {
 	if bitValue(6, byte(l)) == 0 {
 		return Range{
@@ -27,10 +21,14 @@ func (l LCDControl) WindowTileTableAddress() Range {
 	}
 }
 
+// WindowDisplay ...
+// Bit5  Window display                          | ON            | OFF
 func (l LCDControl) WindowDisplay() bool {
 	return bitValue(5, byte(l)) != 0
 }
 
+// TilePatternTableAddress ...
+// Bit4  Tile Pattern Table address              | 8000-8FFF     | 8800-97FF
 func (l LCDControl) TilePatternTableAddress() Range {
 	if bitValue(4, byte(l)) == 0 {
 		return Range{
@@ -42,6 +40,8 @@ func (l LCDControl) TilePatternTableAddress() Range {
 	}
 }
 
+// BackgroundTileTableAddress ...
+// Bit3  Background Tile Table address           | 9C00-9FFF     | 9800-9BFF
 func (l LCDControl) BackgroundTileTableAddress() Range {
 	if bitValue(3, byte(l)) == 0 {
 		return Range{
@@ -53,6 +53,8 @@ func (l LCDControl) BackgroundTileTableAddress() Range {
 	}
 }
 
+// SpriteSize ...
+// Bit2  Sprite size                             | 8x16          | 8x8
 func (l LCDControl) SpriteSize() (byte, byte) {
 	if bitValue(2, byte(l)) == 0 {
 		return 8, 16
@@ -60,10 +62,14 @@ func (l LCDControl) SpriteSize() (byte, byte) {
 	return 8, 8
 }
 
+// WindowTransparencyOnColorZero ...
+// Bit1  Color #0 transparency in the window     | SOLID         | TRANSPARENT
 func (l LCDControl) WindowTransparencyOnColorZero() bool {
 	return bitValue(1, byte(l)) != 0
 }
 
+// BackgroundDisplay ...
+// Bit0  Background display
 func (l LCDControl) BackgroundDisplay() bool {
 	return bitValue(0, byte(l)) != 0
 }
