7334e9decc4564f5fb196bcb295aee98699d3707
Tom Elliott <tom.w.elliott@gmail.com>
2022-02-20 11:48:25 -0500

    Use data bindings

--- cmd/gameboy/fyneui/run.go
+++ cmd/gameboy/fyneui/run.go
@@ -11,6 +11,7 @@ import (
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/data/binding"
 	"fyne.io/fyne/v2/driver/desktop"
 	"fyne.io/fyne/v2/widget"
 	"github.com/theothertomelliott/gameboy"
@@ -22,17 +23,23 @@ func NewUI(gb *gameboy.DMG) *UI {
 	return &UI{
 		gb: gb,
 
-		a:  newMonoLabel("0x00"),
-		f:  newMonoLabel("0x00"),
-		b:  newMonoLabel("0x00"),
-		c:  newMonoLabel("0x00"),
-		d:  newMonoLabel("0x00"),
-		e:  newMonoLabel("0x00"),
-		h:  newMonoLabel("0x00"),
-		l:  newMonoLabel("0x00"),
-		sp: newMonoLabel("0x00"),
-		pc: newMonoLabel("0x00"),
+		a:  stringBinding("0x00"),
+		f:  stringBinding("0x00"),
+		b:  stringBinding("0x00"),
+		c:  stringBinding("0x00"),
+		d:  stringBinding("0x00"),
+		e:  stringBinding("0x00"),
+		h:  stringBinding("0x00"),
+		l:  stringBinding("0x00"),
+		sp: stringBinding("0x00"),
+		pc: stringBinding("0x00"),
 	}
+}
+
+func stringBinding(starting string) binding.String {
+	b := binding.NewString()
+	b.Set("0x00")
+	return b
 }
 
 type UI struct {
@@ -43,22 +50,28 @@ type UI struct {
 	app      fyne.App
 	imgMutex sync.Mutex
 
-	// Register labels
-	a *widget.Label
-	f *widget.Label
-	b *widget.Label
-	c *widget.Label
-	d *widget.Label
-	e *widget.Label
-	h *widget.Label
-	l *widget.Label
+	// Register content
+	a binding.String
+	f binding.String
+	b binding.String
+	c binding.String
+	d binding.String
+	e binding.String
+	h binding.String
+	l binding.String
 
-	sp *widget.Label
-	pc *widget.Label
+	sp binding.String
+	pc binding.String
 }
 
-func newMonoLabel(t string) *widget.Label {
-	l := widget.NewLabel(t)
+func newMonoLabel(s string) *widget.Label {
+	l := widget.NewLabel(s)
+	l.TextStyle.Monospace = true
+	return l
+}
+
+func newMonoLabelWithData(b binding.String) *widget.Label {
+	l := widget.NewLabelWithData(b)
 	l.TextStyle.Monospace = true
 	return l
 }
@@ -130,33 +143,33 @@ func (u *UI) registerState() fyne.CanvasObject {
 		newMonoLabel("Registers"),
 		container.NewHBox(
 			newMonoLabel("A"),
-			u.a,
+			newMonoLabelWithData(u.a),
 			newMonoLabel("F"),
-			u.f,
+			newMonoLabelWithData(u.f),
 		),
 		container.NewHBox(
 			newMonoLabel("B"),
-			u.b,
+			newMonoLabelWithData(u.b),
 			newMonoLabel("C"),
-			u.c,
+			newMonoLabelWithData(u.c),
 		),
 		container.NewHBox(
 			newMonoLabel("D"),
-			u.d,
+			newMonoLabelWithData(u.d),
 			newMonoLabel("E"),
-			u.e,
+			newMonoLabelWithData(u.e),
 		),
 		container.NewHBox(
 			newMonoLabel("H"),
-			u.h,
+			newMonoLabelWithData(u.h),
 			newMonoLabel("L"),
-			u.l,
+			newMonoLabelWithData(u.l),
 		),
 		container.NewHBox(
 			newMonoLabel("SP"),
-			u.sp,
+			newMonoLabelWithData(u.sp),
 			newMonoLabel("PC"),
-			u.pc,
+			newMonoLabelWithData(u.pc),
 		),
 	)
 }
@@ -189,20 +202,20 @@ func (u *UI) drawGraphics(graphics image.Image) {
 }
 
 func (u *UI) updateDebugInfo() {
-	u.a.SetText(fmt.Sprintf("%02X", u.gb.CPU().A.Read8()))
-	u.f.SetText(fmt.Sprintf("%02X", u.gb.CPU().F.Read8()))
+	u.a.Set(fmt.Sprintf("%02X", u.gb.CPU().A.Read8()))
+	u.f.Set(fmt.Sprintf("%02X", u.gb.CPU().F.Read8()))
 
-	u.b.SetText(fmt.Sprintf("%02X", u.gb.CPU().B.Read8()))
-	u.c.SetText(fmt.Sprintf("%02X", u.gb.CPU().C.Read8()))
+	u.b.Set(fmt.Sprintf("%02X", u.gb.CPU().B.Read8()))
+	u.c.Set(fmt.Sprintf("%02X", u.gb.CPU().C.Read8()))
 
-	u.d.SetText(fmt.Sprintf("%02X", u.gb.CPU().D.Read8()))
-	u.e.SetText(fmt.Sprintf("%02X", u.gb.CPU().E.Read8()))
+	u.d.Set(fmt.Sprintf("%02X", u.gb.CPU().D.Read8()))
+	u.e.Set(fmt.Sprintf("%02X", u.gb.CPU().E.Read8()))
 
-	u.h.SetText(fmt.Sprintf("%02X", u.gb.CPU().H.Read8()))
-	u.l.SetText(fmt.Sprintf("%02X", u.gb.CPU().L.Read8()))
+	u.h.Set(fmt.Sprintf("%02X", u.gb.CPU().H.Read8()))
+	u.l.Set(fmt.Sprintf("%02X", u.gb.CPU().L.Read8()))
 
-	u.sp.SetText(fmt.Sprintf("%04X", u.gb.CPU().SP.Read16()))
-	u.pc.SetText(fmt.Sprintf("%04X", u.gb.CPU().PC.Read16()))
+	u.sp.Set(fmt.Sprintf("%04X", u.gb.CPU().SP.Read16()))
+	u.pc.Set(fmt.Sprintf("%04X", u.gb.CPU().PC.Read16()))
 
 	u.win.Content().Refresh()
 }
